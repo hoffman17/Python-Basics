9c8a88a5c37cb114564e0cc773bb766a10b809a7
hoffman17 <84638103+hoffman17@users.noreply.github.com>
2021-05-22 14:06:00 -0400

    Add files via upload

---
+++ game_over.py
@@ -0,0 +1,8 @@
+# The game_over program
+# Utilizes the print function to display information on screen
+# Also utilizes the input function to enable user input during execution
+
+print("Game Over")
+
+input("\n\nPress the enter key to exit.")
+
