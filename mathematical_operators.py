3b63a1462ce8b4fee6e06f4e76bd42547029a7e2
hoffman17 <84638103+hoffman17@users.noreply.github.com>
2021-05-22 15:07:21 -0400

    Add files via upload

---
+++ mathematical_operators.py
@@ -0,0 +1,34 @@
+# The mathematical_operators program
+# Utilizes the different mathematical operators to illustrate their uses
+
+print("The following operators '+', '-', '/', '//', and '%'")
+print("will be used and explained")
+
+# In this example 100 is being added to 20 in the expression and
+# the result displayed
+print("\nWhat is 100 + 20 = ?")
+print(100 + 20)
+
+# In this example 20 is being subtracted from 100 and the result displayed
+print("\nWhat is 100 - 20 = ?")
+print(100 - 20)
+
+# Utilizing the '/' operator conducts "true" division and displays decimals
+# with remainders also known as "floats"
+# In this example 100 is being divided by 20 and the result is being displayed
+print("\nWhat is 100 / 20 =?")
+print(100 / 20)
+
+# In this example 100 is being divided by 20 utilizing integer division only
+# This is akin to saying whole number division and will not show the remainder
+# of the arithmatic if any
+print("\nWhat is 100 // 20 =?")
+print(100 // 20)
+
+# '%' is the modulus operator and displays the remainder after integer division
+# is conducted in an expression
+print("\nWhat is 100 % 19 =?")
+print(100 % 19)
+
+input("\n\nPress the enter key to exit.")
+
